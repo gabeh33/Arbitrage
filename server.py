4dc4c3e378107e9e0ebd11c0b934c761d4cc00b1
Gabriel Holmes <gabeholmes1@gmail.com>
2024-12-27 20:08:31 -0500

    updates

---
+++ server.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+# Add the 'src' directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+from helpers import Tools
+from draftkings import DK
+from flask import Flask
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    try {
+    nba_games = DK.get_nba_events()
+    return nba_games
+    } catch () {
+        return "Unable to access DK"
+    }
+
+if (__name__ == "__main__"):
+    app.run()
